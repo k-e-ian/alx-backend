cb2577da4db8664fa62e4bc0824126a0b5615892
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-19 10:52:13 +0300

    class documentation

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,6 +8,7 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
+    '''inherits from BaseCaching and is a caching system'''
     def put(self, key, item):
         ''' Add an item in the cache '''
         if key is not None and item is not None:
