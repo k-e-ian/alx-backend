adf072295dffeb1aa675deefa51321861ba4400e
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-26 13:47:30 +0300

    parametrization of templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.8
+'''
+File: 3-app.py flask app with babel and parametrize
+'''
+from flask_babel import Babel, gettext
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+
+
+class Config:
+    '''
+    Configuration class for Flask app
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    determine the best-matcing language from the supported ;ang'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    render the 3-index.html template
+    returns: str: rendered HTML content
+    '''
+    return render_template('3-index.html',
+                            title=gettext('home_title'),
+                            header=gettext('home_header'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
